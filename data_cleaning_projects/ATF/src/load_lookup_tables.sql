ea67d6eb975242fb19bd4bc201a1d7d3ac2a5674
sri087 <srikanth.susarapu@outlook.com>
2022-05-31 17:45:53 -0400

    minor corrections

--- data_cleaning_projects/ATF/src/load_lookup_tables.sql
+++ data_cleaning_projects/ATF/src/load_lookup_tables.sql
@@ -5,37 +5,37 @@
 
 -- REGIONS
 
-DROP TABLE regions IF EXISTS
+DROP TABLE IF EXISTS regions;
 
 CREATE TABLE regions (
     region_id smallint,
-    region_name text
+    region_name text,
 
     CONSTRAINT region_key PRIMARY KEY(region_id)
 );
 
-INSERT INTO regions VALUES(
+INSERT INTO regions VALUES
     (1,'Southeast'),
     (3,'Midwest'),
     (4,'Central'),
     (5,'Southwest'),
     (6,'North Atlantic'),
     (8,'Mid Atlantic'),
-    (9,'Western')
-);
+    (9,'Western');
+
 
 -- FEDERAL FIREARM LICENSEE TYPES
 
-DROP TABLE licensee_types IF EXISTS
+DROP TABLE IF EXISTS licensee_types;
 
 CREATE TABLE licensee_types (
     licensee_type_id char(2), 
-    licensee_type_desc text
+    licensee_type_desc text,
 
     CONSTRAINT licensee_type_key PRIMARY KEY(licensee_type_id)
 );
 
-INSERT INTO licensee_types VALUES(
+INSERT INTO licensee_types VALUES
     ('01','Dealer in Firearms Other Than Destructive Devices (Includes Gunsmiths)'),
     ('02','Pawnbroker in Firearms Other Than Destructive Devices'),
     ('03','Collector of Curios and Relics'),
@@ -44,8 +44,8 @@ INSERT INTO licensee_types VALUES(
     ('08','Importer of Firearms Other Than Destructive Devices'),
     ('09','Dealer in Destructive Devices'),
     ('10','Manufacturer of Destructive Devices'),
-    ('11','Importer of Destructive Devices')
-);
+    ('11','Importer of Destructive Devices');
+
 
 
 -- FEDERAL FIREARM LICENSE EXPIRATION DATE
@@ -57,16 +57,16 @@ INSERT INTO licensee_types VALUES(
 -- The second character is a letter representing the month of the year the FFL expires. The months and their corresponding letters (“I” is skipped) are:
 -- A – January  B – February  C – March  D – April  E – May  F – June  G – July  H – August  J – September  K – October  L – November   M – December
 
-DROP TABLE license_exp_month IF EXISTS
+DROP TABLE IF EXISTS license_exp_month;
 
 CREATE TABLE license_exp_month (
     license_exp_mth_code char(1), 
-    license_exp_mth text
+    license_exp_mth text,
     
     CONSTRAINT license_exp_mth_code_key PRIMARY KEY(license_exp_mth_code)
 );
 
-INSERT INTO license_exp_month VALUES(
+INSERT INTO license_exp_month VALUES
     ('A','January'),
     ('B','February'),
     ('C','March'),
@@ -78,5 +78,9 @@ INSERT INTO license_exp_month VALUES(
     ('J','September'),
     ('K','October'),
     ('L','November'),
-    ('M','December')
-);
+    ('M','December');
+
+
+SELECT * from regions;
+SELECT * from licensee_types;
+SELECT * from license_exp_month;
